2cbc48e66d21d6fc330f66c4aa94c507c7bb3711
Asael Gonzalez <agonzalezengr@gmail.com>
2022-09-15 16:59:07 -0500

    making new branch 'qna'

--- server/models/qna.js
+++ server/models/qna.js
@@ -1,1 +1,27 @@
+CREATE TABLE IF NOT EXISTS questions (
+  id SERIAL PRIMARY KEY,
+  product_id SERIAL,
+  body VARCHAR,
+  date_written TEXT,
+  asker_name VARCHAR,
+  asker_email VARCHAR,
+  reported BOOL,
+  helpful SERIAL
+);
 
+CREATE TABLE IF NOT EXISTS answers (
+  id SERIAL PRIMARY KEY,
+  question_id SERIAL REFERENCES questions (id),
+  body VARCHAR,
+  date_written TEXT,
+  answerer_name VARCHAR,
+  answerer_email VARCHAR,
+  reported BOOL,
+  helpful SERIAL
+);
+
+CREATE TABLE IF NOT EXIST answer_photos (
+  id SERIAL PRIMARY KEY,
+  answer_id SERIAL REFERENCES answers (id),
+  url TEXT
+)
