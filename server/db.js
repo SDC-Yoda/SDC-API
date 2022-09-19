a128da6d7a391eb8dc5e7c424bfefc9c3db0d56e
Tanner White <tannerjwhite618@gmail.com>
2022-09-19 15:02:44 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool ({
-  user: 'tannerwhite',
-  host: 'localhost',
-  database: 'practice',
-  password: 'null',
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
 });
 
