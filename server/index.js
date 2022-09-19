e8325b4883c354c68f380cc38d8b0a050c4ea383
Tanner White <tannerjwhite618@gmail.com>
2022-09-19 15:01:46 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,20 +1,29 @@
 const prodCont = require('./controllers/products.js')
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
-app.get('/products/:product_id', (req, res) => {
-  const query = {
-    text: `select * from products where id = $1`,
-    values: [req.params.product_id]
-  }
-  client.query(query)
-    .then((response) => {
-      res.send(response.rows[0]);
-    })
-})
-app.get('/products/:product_id/styles', (req, res) => { prodCont.getStyle(req, res); })
+app.use(cors());
+app.use(express.json());
 
-app.listen(8000, () => {
+app.get('/products', (req, res) => {
+  prodCont.getProducts(req, res);
+})
+app.get('/products/:page&:quantity', (req, res) => {
+  prodCont.getProducts(req, res);
+})
+app.get('/products/:product_id', (req, res) => {
+  prodCont.getProduct(req, res);
+})
+app.get('/products/:product_id/styles', (req, res) => {
+  prodCont.getStyle(req, res);
+})
+app.get('/products/:product_id/related', (req, res) => {
+  prodCont.getRelated(req, res);
+})
+
+
+app.listen(8000 , () => {
   console.log('Server listening on 8000');
 })
